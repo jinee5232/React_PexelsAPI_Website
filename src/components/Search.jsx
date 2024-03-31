183f88b8601644a1a05a9bd6bcff07feaf74d097
jinee5232@gmail.com <jingyee0827@gmail.com>
2024-03-31 17:03:44 +0800

    enter search

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,9 +4,19 @@ const Search = ({ search, setInput }) => {
   const Handleinput = (e) => {
     setInput(e.target.value);
   };
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      search();
+    }
+  };
   return (
     <div className="search">
-      <input className="input" type="text" onChange={Handleinput} />
+      <input
+        className="input"
+        type="text"
+        onChange={Handleinput}
+        onKeyDown={handleKeyPress}
+      />
       <button onClick={search}>Search</button>
     </div>
   );
